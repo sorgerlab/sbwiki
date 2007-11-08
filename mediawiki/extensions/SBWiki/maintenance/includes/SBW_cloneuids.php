dd9a91f61873eda5dffce6e98b981724897eec79
jlm26 <jlm26@51f1fa5e-573d-0410-b3da-fc4600a00759>
2007-11-08 00:30:10 +0000

    finish uid clone script
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/sorger/sbwiki/trunk@29 51f1fa5e-573d-0410-b3da-fc4600a00759

--- mediawiki/extensions/SBWiki/maintenance/includes/SBW_cloneuids.php
+++ mediawiki/extensions/SBWiki/maintenance/includes/SBW_cloneuids.php
@@ -8,7 +8,7 @@ function sbwfCloneUIDs( $old_uids ) {
   for ( $i = 0; $i < count($old_uids); $i++ ) {
     $old_uid = sbwfParseUID($old_uids[$i]);
     // reuse parts of old uid, except index 2 which is the id number
-    $new_uid = "$old_uid[0]-$old_uid[1]-XXXX-$old_uid[3]"; #sbwfAllocateUID($old_uid[0], $old_uid[1], $old_uid[3]);
+    $new_uid = sbwfAllocateUID($old_uid[0], $old_uid[1], $old_uid[3]);
     $new_uids[] = $new_uid;
     $wgOut->addHTML("$i: $old_uids[$i] -> $new_uid\n");
   }
