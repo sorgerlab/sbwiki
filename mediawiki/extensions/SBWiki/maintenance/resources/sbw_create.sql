a9d401a5ce898e77498d0be91aab94922a8318ff
jlm26 <jlm26@51f1fa5e-573d-0410-b3da-fc4600a00759>
2007-11-08 00:29:57 +0000

    force uid id numbers to start at 100 (just for aesthetic purposes)
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/sorger/sbwiki/trunk@28 51f1fa5e-573d-0410-b3da-fc4600a00759

--- mediawiki/extensions/SBWiki/maintenance/resources/sbw_create.sql
+++ mediawiki/extensions/SBWiki/maintenance/resources/sbw_create.sql
@@ -1,8 +1,19 @@
 CREATE TABLE /*$wgDBprefix*/sbw_uid (
-  id                    bigint unsigned NOT NULL        auto_increment,
+  id                    serial,
   type_code             char(2)         NOT NULL,
   creator_initials      char(3)         NOT NULL,
   annotation            varchar(255),
 
   PRIMARY KEY           id (id)
 ) /*$wgDBTableOptions*/;
+
+
+-- Force id numbering to start at 100 (just an aesthetic choice).
+--
+DELETE FROM /*$wgDBprefix*/sbw_uid
+  WHERE id=99;
+INSERT INTO /*$wgDBprefix*/sbw_uid
+  (id, type_code, creator_initials, annotation)
+  VALUES (99, 'XX', 'XXX', 'dummy');
+DELETE FROM /*$wgDBprefix*/sbw_uid
+  WHERE id=99;
