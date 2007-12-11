ee0fa21606bf81d06a2e4178510f4c5d9144474f
jlm26 <jlm26@51f1fa5e-573d-0410-b3da-fc4600a00759>
2007-12-11 21:16:27 +0000

    - actually translate bibtex citations to wikitext
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/sorger/sbwiki/trunk@36 51f1fa5e-573d-0410-b3da-fc4600a00759

--- import_tools/boolean_jsr/filter_bibtexref2wikitext.pl
+++ import_tools/boolean_jsr/filter_bibtexref2wikitext.pl
@@ -2,7 +2,16 @@
 
 use strict;
 use Text::BibTeX;
+use POSIX;
+use XML::Twig;
 use Data::Dumper;
+
+
+use constant {
+  CITE_BEGIN => '\cite{',
+  CITE_END   => '}',
+};
+
 
 
 my ($input_file, $bibtex_file) = @ARGV;
@@ -12,12 +21,47 @@ if (!defined $input_file or !defined $bibtex_file )
 }
 
 
-my %citations = build_citation_dict($bibtex_file);
-print Dumper(\%citations);
+my %citation_refs = build_citation_dict($bibtex_file);
 
-open(IN, $input_file) or die "$input_file: $!\n";
-close(IN);
+my $twig = XML::Twig->new(pretty_print => 'indented');
+$twig->parsefile($input_file);
 
+foreach my $text_elt ( $twig->findnodes('/mediawiki/page/revision/text') )
+{
+  my $input_content = $text_elt->text;
+
+  my $start_pos;
+  my $end_pos = 0;
+
+  while ( ($start_pos = index($input_content, CITE_BEGIN, $end_pos)) >= 0 )
+  {
+    $end_pos = index($input_content, CITE_END, $start_pos);
+    my $ids_start     = $start_pos + length(CITE_BEGIN);
+    my $ids_end       = $end_pos - $ids_start + 1 - length(CITE_END);
+    my $citation_text = substr($input_content, $ids_start, $ids_end);
+    my @citation_ids      = map { s/\s//g; $_ } split(/,/, $citation_text);
+    my @citation_wikitext;
+    foreach my $id ( @citation_ids )
+    {
+      my $wikitext = $citation_refs{$id};
+      defined $wikitext or die "Citation id not found in bibtex file: {$id}\n(context: $citation_text)\n";
+      push @citation_wikitext, $citation_refs{$id};
+    }
+    my $new_wikitext = " (" . join(", ", @citation_wikitext) . ") ";
+    substr($input_content, $start_pos, $end_pos - $start_pos + 1) = $new_wikitext;
+    $end_pos = $start_pos + length($new_wikitext);
+  }
+
+  $text_elt->set_text($input_content);
+}
+
+my $timestamp = POSIX::strftime('%Y-%m-%dT%H:%M:%SZ', gmtime);
+foreach my $timestamp_elt ( $twig->findnodes('/mediawiki/page/revision/timestamp') )
+{
+  $timestamp_elt->set_text($timestamp);
+}
+
+$twig->flush;
 
 
 #############################################
@@ -39,7 +83,11 @@ sub build_citation_dict
     my $wikitext = "UNKNOWN REFERENCE";
     if ( $entry->exists('pmid') )
     {
-      $wikitext = $entry->get('pmid');
+      $wikitext = "PMID " . $entry->get('pmid');
+    }
+    elsif ( $entry->exists('doi') )
+    {
+      $wikitext = "[[http://dx.doi.org/" . $entry->get('doi') . "]]";
     }
     elsif ( $entry->exists('isbn') )
     {
