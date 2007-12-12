a44636369bdde968ff4c3a76a0c5d518f4290cb0
jlm26 <jlm26@51f1fa5e-573d-0410-b3da-fc4600a00759>
2007-12-12 21:25:40 +0000

    - added preliminary reference formatting for the reference= template parameter
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/sorger/sbwiki/trunk@37 51f1fa5e-573d-0410-b3da-fc4600a00759

--- import_tools/boolean_jsr/filter_bibtexref2wikitext.pl
+++ import_tools/boolean_jsr/filter_bibtexref2wikitext.pl
@@ -21,7 +21,7 @@ if (!defined $input_file or !defined $bibtex_file )
 }
 
 
-my %citation_refs = build_citation_dict($bibtex_file);
+my ($citations, $references) = parse_references($bibtex_file);
 
 my $twig = XML::Twig->new(pretty_print => 'indented');
 $twig->parsefile($input_file);
@@ -32,6 +32,7 @@ foreach my $text_elt ( $twig->findnodes('/mediawiki/page/revision/text') )
 
   my $start_pos;
   my $end_pos = 0;
+  my %seen_refs;
 
   while ( ($start_pos = index($input_content, CITE_BEGIN, $end_pos)) >= 0 )
   {
@@ -40,17 +41,22 @@ foreach my $text_elt ( $twig->findnodes('/mediawiki/page/revision/text') )
     my $ids_end       = $end_pos - $ids_start + 1 - length(CITE_END);
     my $citation_text = substr($input_content, $ids_start, $ids_end);
     my @citation_ids      = map { s/\s//g; $_ } split(/,/, $citation_text);
+    @seen_refs{@citation_ids} = (1) x @citation_ids;
     my @citation_wikitext;
     foreach my $id ( @citation_ids )
     {
-      my $wikitext = $citation_refs{$id};
-      defined $wikitext or die "Citation id not found in bibtex file: {$id}\n(context: $citation_text)\n";
-      push @citation_wikitext, $citation_refs{$id};
+      defined $citations->{$id} or die "Citation id not found in bibtex file: {$id}\n(context: $citation_text)\n";
+      push @citation_wikitext, $citations->{$id} ;
     }
-    my $new_wikitext = " (" . join(", ", @citation_wikitext) . ") ";
-    substr($input_content, $start_pos, $end_pos - $start_pos + 1) = $new_wikitext;
-    $end_pos = $start_pos + length($new_wikitext);
+    my $cite_wikitext = " (" . join(", ", @citation_wikitext) . ") ";
+    substr($input_content, $start_pos, $end_pos - $start_pos + 1) = $cite_wikitext;
+    $end_pos = $start_pos + length($cite_wikitext);
   }
+
+  my $ref_text = join("\n\n", @$references{keys %seen_refs});
+  $ref_text =~ s/\|/&#124;/g;
+  $ref_text =~ tr/{}//d;
+  $input_content =~ s/(references=)/$1$ref_text/;
 
   $text_elt->set_text($input_content);
 }
@@ -66,12 +72,14 @@ $twig->flush;
 
 #############################################
 
-sub build_citation_dict
+sub parse_references
 {
   my ($bibtex_file) = @_;
 
   my $bibfile = Text::BibTeX::File->new($bibtex_file);
+  $bibfile->set_structure('Bib');
   my %citations;
+  my %references;
   while ( my $entry = Text::BibTeX::Entry->new($bibfile) )
   {
     if ( !$entry->parse_ok )
@@ -110,7 +118,48 @@ sub build_citation_dict
     }
 
     $citations{$entry->key} = $wikitext;
+    $references{$entry->key} = format_reference($entry);
   }
 
-  return %citations;
+  return \%citations, \%references;
+}
+
+
+sub format_reference
+{
+  my ($entry) = @_;
+
+  my @blocks = $entry->format;
+
+  # the following was borrowed from btformat in the Text::BibTeX distribution
+ BLOCK:
+  for my $block (@blocks)
+  {
+  SENTENCE:
+    for my $sentence (@$block)
+    {
+      # If sentence has multiple clauses, process them: first, strip
+      # out empties, and jump to the next sentence if it turns out 
+      # this one is empty (ie. just a bunch of empty clauses).  Then
+      # join the left-over clauses with commas.
+      if (ref $sentence eq 'ARRAY')
+      {
+        @$sentence = grep ($_, @$sentence);
+        ($sentence = '', next SENTENCE) unless @$sentence;
+        $sentence = join (', ', @$sentence);
+      }
+
+      # finish sentence with a period if it's not already punctuated
+      $sentence .= '.' unless $sentence eq '' || $sentence =~ /[.!?]$/;
+    }
+
+    # Now join together all the sentences in the block, first stripping
+    # any empties.
+    @$block = grep ($_, @$block);
+    next BLOCK unless @$block;
+    $block = join (' ', @$block);     # put the sentences together
+  }
+  my $reference = join(' ', @blocks);
+
+  return $reference;
 }
