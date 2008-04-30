dddde8b1a6b0cc53b1dde617f9dfaa352c95dc3a
jlm26 <jlm26@51f1fa5e-573d-0410-b3da-fc4600a00759>
2008-04-30 20:28:49 +0000

    improved python owl->mw script a little
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/sorger/sbwiki/trunk@55 51f1fa5e-573d-0410-b3da-fc4600a00759

--- ontology/wiki_importer/owl2mwxml.py
+++ ontology/wiki_importer/owl2mwxml.py
@@ -8,13 +8,20 @@ if len(sys.argv) <= 1:
     print "ERROR: must specify an .owl file to parse";
     sys.exit(1);
 
+RDF = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+RDFS = Namespace('http://www.w3.org/2000/01/rdf-schema#')
+XSD = Namespace('http://www.w3.org/2001/XMLSchema#')
+OWL = Namespace('http://www.w3.org/2002/07/owl#')
+
 graph = Graph()
 graph.load(sys.argv[1])
 
-RDF_NS = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
-OWL_NS = Namespace('http://www.w3.org/2002/07/owl#');
+# extract the ontology's namespace uri
+onto_base = graph.value(predicate=RDF['type'], object=OWL['Ontology'], any=False)
+# XXX: Is this safe?  Might also be "/"... Does Protege always use "#"?
+ONTO = Namespace(onto_base + "#")
 
-#print graph.serialize(format='turtle')
-
-for s,p,o in graph.triples((None, RDF_NS['type'], OWL_NS['Class'])): 
-    print s
+for s,p,o in graph.triples((None, RDF['type'], OWL['Class'])):
+    if s.startswith(ONTO):
+        label = graph.value(s, RDFS['label'], None, any=False)
+        print label
