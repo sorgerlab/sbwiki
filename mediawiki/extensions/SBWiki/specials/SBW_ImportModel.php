2aef20e54d3d2d0d20644cfba76641216ad52081
jlm26 <jlm26@51f1fa5e-573d-0410-b3da-fc4600a00759>
2009-02-03 21:41:06 +0000

    completed basic model import functionality
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/sorger/sbwiki/trunk@105 51f1fa5e-573d-0410-b3da-fc4600a00759

--- mediawiki/extensions/SBWiki/specials/SBW_ImportModel.php
+++ mediawiki/extensions/SBWiki/specials/SBW_ImportModel.php
@@ -15,25 +15,29 @@ function doSpecialImportModel() {
   global $wgOut, $wgRequest, $wgScriptPath, $smwgScriptPath;
   $fname = 'SBW::doSpecialImportModel';
 
-  $submitted        = $wgRequest->getVal('import');
+  $do_preview       = $wgRequest->getVal('preview');
+  $do_import        = $wgRequest->getVal('import');
   $creator_initials = $wgRequest->getVal('creator_initials');
   $annotation       = $wgRequest->getVal('annotation');
   $model_contents   = $wgRequest->getVal('model_contents');
 
   $errors = array();
 
-  if ( !$submitted and $user_initials = sbwfGetUserInitials() ) {
-    $creator_initials = $user_initials;
-  }
-  if ( $submitted ) {
+  if ( $wgRequest->wasPosted() ) {
     if ( !$creator_initials ) $errors[] = '<em>Creator Initials</em> not specified';
     if ( !$model_contents   ) $errors[] = '<em>SBML Code</em> not provided';
+  } elseif ( $user_initials = sbwfGetUserInitials() ) {
+    $creator_initials = $user_initials;
   }
 
 
-  if ( $wgRequest->wasPosted() and $submitted and empty($errors) ) {
-    // successful form submission (but the SBML might still be invalid)
-    renderPreview($model_contents, $creator_initials, $annotation);
+  if ( $wgRequest->wasPosted() and empty($errors) ) {
+    // successful form submission
+    if ( $do_preview ) {
+      renderPreview($model_contents, $creator_initials, $annotation);
+    } elseif ( $do_import ) {
+      importModel($model_contents, $creator_initials, $annotation);
+    }
   } else {
     // first visit or error on submission
     renderUploadForm($errors, $creator_initials, $annotation);
@@ -64,7 +68,7 @@ function renderUploadForm($errors, $creator_initials, $annotation)
 <tr><td><strong>Creator Initials:</strong></td><td><input name="creator_initials" type="text" value="$creator_initials"></td></tr>
 <tr><td><strong>Annotation (optional):</strong></td><td><input name="annotation" type="text" value="$annotation"></td></tr>
 <tr><td><strong>SBML Code<br/>(paste file contents):</strong></td><td><textarea cols="50" rows="20" name="model_contents"></textarea></td></tr>
-<tr><td></td><td><input name="import" type="submit" value="Import Model"></td></tr>
+<tr><td></td><td><input name="preview" type="submit" value="Continue"></td></tr>
 </table>
 
 </form>
@@ -77,14 +81,14 @@ FORM
 
 function renderPreview($model_contents, $creator_initials, $model_annotation)
 {
-  global $wgOut, $smwgScriptPath;
+  global $wgOut, $wgScriptPath, $smwgScriptPath;
 
   $parser = new SBWSbmlReader($model_contents);
   $model = $parser->getModel();
 
   // assign fake UIDs for preview display purposes
   $fake_counter = 100;
-  $model->uid = sbwfFormatUID('MD', $creator_initials, $fake_counter++, $annotation);
+  $model->uid = sbwfFormatUID('MD', $creator_initials, $fake_counter++, $model_annotation);
   foreach ( $model->getSpeciesIds() as $id ) {
     $species = $model->getSpecies($id);
     $species->uid = sbwfFormatUID('SP', $creator_initials, $fake_counter++, $species->getBestName());
@@ -98,6 +102,10 @@ function renderPreview($model_contents, $creator_initials, $model_annotation)
     $parameter->uid = sbwfFormatUID('PA', $creator_initials, $fake_counter++, $parameter->getBestName());
   }
 
+  $model_contents   = htmlspecialchars($model_contents);
+  $creator_initials = htmlspecialchars($creator_initials);
+  $model_annotation = htmlspecialchars($model_annotation);
+
   $formatter = new SBWModelFormatter($parser->getModel());
 # FIXME: SMW could change this... probably better to copy it and make our own style
   $wgOut->addHeadItem('smw_css',
@@ -105,7 +113,8 @@ function renderPreview($model_contents, $creator_initials, $model_annotation)
 		      $smwgScriptPath . '/skins/SMW_custom.css" />' . "\n");
   $wgOut->addWikiText(<<<INTRO
 Below is a preview of what your model will look like in the wiki.
-'''DO NOT CLICK''' on any of the links!
+'''DO NOT CLICK''' on any of the links!  If you are satisfied with the preview,
+click the <strong>Import</strong> button at the bottom of this page.
 
 ----
 
@@ -113,6 +122,68 @@ Below is a preview of what your model will look like in the wiki.
 INTRO
                         );
   $wgOut->addWikiText($formatter->formatAll());
+
+  $wgOut->addHTML(<<<FORM
+<form method="post" action="$wgScriptPath/index.php/Special:ImportModel">
+<input name="creator_initials" type="hidden" value="$creator_initials">
+<input name="annotation" type="hidden" value="$model_annotation">
+<input name="model_contents" type="hidden" value="$model_contents">
+<input name="import" type="submit" value="Import">
+</form>
+FORM
+		  );
+}
+
+
+function importModel($model_contents, $creator_initials, $model_annotation)
+{
+  global $wgOut;
+
+  $parser = new SBWSbmlReader($model_contents);
+  $model = $parser->getModel();
+  $entities = array();
+
+  $model->uid = sbwfAllocateUID('MD', $creator_initials, $model_annotation);
+  $entities[] = $model;
+
+  foreach ( $model->getSpeciesIds() as $id ) {
+    $species = $model->getSpecies($id);
+    $species->uid = sbwfAllocateUID('SP', $creator_initials, $species->getBestName());
+    $entities[] = $species;
+  }
+  foreach ( $model->getReactionIds() as $id ) {
+    $reaction = $model->getReaction($id);
+    $reaction->uid = sbwfAllocateUID('IX', $creator_initials, $reaction->getBestName());
+    $entities[] = $reaction;
+  }
+  foreach ( $model->getParameterIds() as $id ) {
+    $parameter = $model->getParameter($id);
+    $parameter->uid = sbwfAllocateUID('PA', $creator_initials, $parameter->getBestName());
+    $entities[] = $parameter;
+  }
+
+  $formatter = new SBWModelFormatter($model);
+  $success_titles = array();
+  $error_titles = array();
+  foreach ( $entities as $entity ) {
+    $title = Title::newFromText($entity->uid);
+    $article = new Article($title);
+    $text = $formatter->format($entity);
+    $result = $article->doEdit($text, "model import");
+    if ( $result ) {
+      $success_titles[] = $title;
+    } else {
+      $error_titles[] = $title;
+    }
+  }
+
+  if ( !$error_titles ) {
+    $wgOut->addWikiText("Model imported successfully.  The following pages were created:\n");
+    foreach ($success_titles as $title) $wgOut->addWikiText("* [[$title]]\n");
+  } else {
+    $wgOut->addWikiText("Errors during import on the following pages:\n");
+    foreach ($error_titles as $title) $wgOut->addWikiText("* $title\n");
+  }
 }
 
 
