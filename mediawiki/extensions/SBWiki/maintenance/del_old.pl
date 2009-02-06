84cd25bb3135a9b1d7cbd5cf2472b30e74a1c72f
jlm26 <jlm26@51f1fa5e-573d-0410-b3da-fc4600a00759>
2009-02-06 20:55:50 +0000

    modified del_old.pl to take username/password via ReadLine
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/sorger/sbwiki/trunk@106 51f1fa5e-573d-0410-b3da-fc4600a00759

--- mediawiki/extensions/SBWiki/maintenance/del_old.pl
+++ mediawiki/extensions/SBWiki/maintenance/del_old.pl
@@ -2,17 +2,27 @@
 
 use strict;
 use IO::All;
+use Term::ReadLine;
 use Perlwikipedia;
 
 
+
+my $term = Term::ReadLine->new;
+#		            my $OUT = $term->OUT || \*STDOUT;
+my $username = $term->readline('wiki username:');
+my $password = $term->readline('wiki password:');
+print STDERR "reading page titles from stdin...\n";
+
 my $wiki = Perlwikipedia->new;
 $wiki->set_wiki('https://dev.pipeline.med.harvard.edu', '/wiki');
-$wiki->login('Username', 'password') == 0
+$wiki->login($username, $password) == 0
   or die "error: could not log into wiki:\n", $wiki->{errstr};
 
-my @pages = io('pages')->chomp->slurp;
+my @pages = io('-')->slurp;
 foreach my $page (@pages)
 {
+  $page =~ s/\s+$//;
+  $page =~ s/^\s+//;
   print "$page\n";
   $wiki->get_text($page) or next;
   $wiki->delete_page($page, 'failed import');
