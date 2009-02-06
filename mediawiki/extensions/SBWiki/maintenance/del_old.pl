e0dfbfd64c2220946c217dc0893551a642de9665
jlm26 <jlm26@51f1fa5e-573d-0410-b3da-fc4600a00759>
2009-02-06 21:03:33 +0000

    mask password entry
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/sorger/sbwiki/trunk@107 51f1fa5e-573d-0410-b3da-fc4600a00759

--- mediawiki/extensions/SBWiki/maintenance/del_old.pl
+++ mediawiki/extensions/SBWiki/maintenance/del_old.pl
@@ -7,17 +7,18 @@ use Perlwikipedia;
 
 
 
-my $term = Term::ReadLine->new;
-#		            my $OUT = $term->OUT || \*STDOUT;
-my $username = $term->readline('wiki username:');
-my $password = $term->readline('wiki password:');
-print STDERR "reading page titles from stdin...\n";
+my $term = Term::ReadLine->new('del_old');
+#my $OUT = $term->OUT || \*STDOUT;
+my $username = $term->readline('wiki username: ');
+$term->Attribs->{redisplay_function} = $term->Attribs->{shadow_redisplay};
+my $password = $term->readline('wiki password: ');
 
 my $wiki = Perlwikipedia->new;
 $wiki->set_wiki('https://dev.pipeline.med.harvard.edu', '/wiki');
 $wiki->login($username, $password) == 0
   or die "error: could not log into wiki:\n", $wiki->{errstr};
 
+print STDERR "reading page titles from stdin...\n";
 my @pages = io('-')->slurp;
 foreach my $page (@pages)
 {
