9767912b43796e231f430f484f763568e5f3250b
jlm26 <jlm26@51f1fa5e-573d-0410-b3da-fc4600a00759>
2010-01-31 00:05:52 +0000

    improve presentation of name differences
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/sorger/sbwiki/trunk@122 51f1fa5e-573d-0410-b3da-fc4600a00759

--- tools/obo2dot.pl
+++ tools/obo2dot.pl
@@ -37,31 +37,60 @@ $Text::Wrap::separator = '<br/>';
 print "digraph OBO {\n";
 print <<GOPTS;
 rankdir=RL
+node [fontname="DejaVu Sans Condensed"]
 GOPTS
+
+my $BEGIN_WORD = qr/(?=\W)/ms;
 
 foreach my $node (values %nodes) {
   my $id = $node->{id};
   my $label = $node->{label};
   my @parents = @{$node->{parents}};
+  my $parent_label = $nodes{$parents[0]}->{label};
 
-  if (length($label) > $Text::Wrap::columns and @parents == 1) {
-    $label = word_diff(\$nodes{$parents[0]}->{label}, \$label, {STYLE => 'HTML'});
-    $label =~ s|</?span[^>]*>||g;
-    $label =~ s|</?div[^>]*>||g;
-    $label =~ s|<ins>|\x01|g;
-    $label =~ s|</ins>|\x02|g;
-    $label =~ s|<del>|\x03|g;
-    $label =~ s|</del>|\x04|g;
-    $label = wrap('', '', $label);
-    $label =~ s|\x01|<font color="#00ff00">|g;
-    $label =~ s|\x02|</font>|g;
-    $label =~ s|\x03|<font color="#ff0000">|g;
-    $label =~ s|\x04|</font>|g;
+  my $new_label;
+
+  if (length($label) > $Text::Wrap::columns and @parents == 1 and length($parent_label) > $Text::Wrap::columns) {
+
+    foreach my $str ($label, $parent_label)
+    {
+      $str = ' ' . $str;
+    }
+    my @words_node = split($BEGIN_WORD, $label);
+    my @words_parent = split($BEGIN_WORD, $parent_label);
+    # FIXME debugging, remove
+    if ($id eq 'SBO_0000426' or $id eq 'SBO_0000086') {
+      use Data::Dumper;
+      #print STDERR Dumper(\@words_node, \@words_parent);
+    }
+    $new_label = word_diff(\@words_parent, \@words_node, {STYLE => 'HTML'});
+    $new_label =~ s|</?span[^>]*>||g;
+    $new_label =~ s|</?div[^>]*>||g;
+    $new_label =~ s|<ins>|\x01|g;
+    $new_label =~ s|</ins>|\x02|g;
+    $new_label =~ s|<del>|\x03|g;
+    $new_label =~ s|</del>|\x04|g;
+
+    my $unchanged = $new_label;
+    $unchanged =~ s/\x01[^\x02]+\x02//g;
+    $unchanged =~ s/\x03[^\x04]+\x04//g;
+    if (length($unchanged) < length($label) / 2) {
+      $new_label = $label;
+    }
+
+    $new_label = wrap('', '', $new_label);
+    $new_label =~ s|\x01|<font color="#00e000" face="DejaVu Sans Condensed Bold">|g;
+    $new_label =~ s|\x02|</font>|g;
+    $new_label =~ s|\x03|<font color="#ff8080" face="DejaVu Sans Condensed Italic">|g;
+    $new_label =~ s|\x04|</font>|g;
+
   } else {
-    $label = wrap('', '', $label);
+
+    $new_label = wrap('', '', $label);
+
   }
 
-  print "${id}[label=<$label>]\n";
+  print "${id}[label=<$new_label>]\n";
   foreach my $parent (@parents) {
     print "$id -> $parent\n"
   }
