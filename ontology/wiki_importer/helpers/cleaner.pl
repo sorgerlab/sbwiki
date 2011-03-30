6003703c2bf2ca6d8aab9e21c9b5e63caa027fcb
jlm26 <jlm26@51f1fa5e-573d-0410-b3da-fc4600a00759>
2011-03-30 18:46:07 +0000

    updated cleaner helper script
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/sorger/sbwiki/trunk@169 51f1fa5e-573d-0410-b3da-fc4600a00759

--- ontology/wiki_importer/helpers/cleaner.pl
+++ ontology/wiki_importer/helpers/cleaner.pl
@@ -1,20 +1,55 @@
 #!/usr/bin/perl
 
 use strict;
-use Perlwikipedia;
+use MediaWiki::Bot;
 
 
-my $wiki = Perlwikipedia->new;
-$wiki->set_wiki('https://dev.pipeline.med.harvard.edu', '/wiki');
-$wiki->login('Ontologybot', undef) == 0
-  or die "error: could not log into wiki:\n", $wiki->{errstr};
+my $protocol = 'https';
+my $host = 'dev.pipeline.med.harvard.edu';
+my $path = 'wiki';
+my $username = 'Ontologybot';
+my $password = 'bot7';
+print "connecting to $protocol://$username\@$password:$host/$path\n\n";
+my $bot = MediaWiki::Bot->new({
+  protocol => $protocol,
+  host => $host,
+  path => $path,
+  login_data => { username => $username, password => $password },
+  debug => 2,
+});
+$bot or die "error: could not log into wiki:\n", $bot->{errstr};
 
-my %namespaces = reverse $wiki->get_namespace_names;
+my %namespaces = reverse $bot->get_namespace_names;
 
-my @pages = $wiki->get_pages_in_namespace($namespaces{Template}, 5000);
-foreach my $page (@pages)
+print "deleting pages\n=====\n";
+
+foreach my $page ($bot->get_pages_in_namespace($namespaces{Category}, 5000))
 {
-#  next unless $page =~ /^Template:Property:/;
-#  print "$page\n";
-#  $wiki->delete_page($page, 'poor naming choice');
+  print "$page\n";
+  $DB::single=1;
+  $bot->delete($page, 'cleaning up ontology imports');
+  check_error();
+}
+
+foreach my $page ($bot->get_pages_in_namespace($namespaces{Template}, 5000))
+{
+  next if $page !~ /^Template:(Category|Property|PropertyPrefix) /;
+  print "$page\n";
+  $bot->delete($page, 'cleaning up ontology imports');
+}
+
+foreach my $page ($bot->get_pages_in_namespace($namespaces{Property}, 5000))
+{
+  next if grep($page eq "Property:$_", qw(Initials));
+  print "$page\n";
+  $bot->delete($page, 'cleaning up ontology imports');
+}
+
+
+sub check_error
+{
+  if (defined $bot->{error})
+  {
+    print "ERROR (#$bot->{error}{code}) $bot->{error}{details}\n";
+  }
 }
